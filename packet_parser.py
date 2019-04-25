c30f3ea2aa3452a7e253c6ef4268024d87d7da10
Micah J. Martin <mjm5097@rit.edu>
2019-04-25 15:59:22 -0400

    Added class for the data

--- packet_parser.py
+++ packet_parser.py
@@ -1,4 +1,21 @@
-def parse() :
-	print 'called parse function in packet_parser.py'
+from __future__ import print_function
 
-	
+class Icmp(object):
+	def __init__(self, data):
+		data = data.strip().split()
+		self.num = int(data[0]) # Num of packet
+		self.time = float(data[1])
+		self.src = data[2] # Source IP
+		self.dst = data[3] # Dest IP
+		self.length = int(data[5])
+		self.info = " ".join(data[6:])
+		print(self.__dict__)
+	def __str__(self):
+		return " ".join([str(self.num), str(self.time), self.src, self.dst, str(self.length), self.info])
+
+def parse():
+	data = "441 590.404752     192.168.100.1         192.168.100.2         ICMP     74     Echo (ping) request  id=0x0001, seq=91/23296, ttl=128 (reply in 442)"
+	x = Icmp(data)
+	print(x)
+
+parse()
